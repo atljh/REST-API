a0280e6e7196ca5d2608535eb380f41e36ffbc6d
atljh <92812073+atljh@users.noreply.github.com>
2022-03-24 17:44:28 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,78 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import cherrypy
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Router(object):
+    def __init__(self):
+        self.albums = Album()
+        self.song = Song()
+        self.band = Band()
+        self.everyone = Everyone()
+        self.search = Search()
+
+    def _cp_dispatch(self, vpath):
+        if len(vpath) == 0:  # /
+            return self.everyone
+
+        if len(vpath) == 1:  # /search
+            resource_type = vpath.pop(0)
+            if resource_type == 'search':
+                return self.search
+            else:
+                return 'unknown type'
+
+        if len(vpath) == 2:  # /artist/<artist>
+            vpath.pop(0)
+            cherrypy.request.params['name'] = vpath.pop()
+            return self.band
+
+        if len(vpath) == 4:  # /artist/<artist>/song/<song>
+            vpath.pop(0)
+            cherrypy.request.params['artist'] = vpath.pop(0)  # artist name
+            resource_type = vpath.pop(0)
+
+            if resource_type == 'song':
+                cherrypy.request.params['name'] = vpath.pop(0)  # /song name/
+                return self.song
+
+            elif resource_type == 'album':
+                cherrypy.request.params['title'] = vpath.pop(0)  # /album title/
+                return self.albums
+
+            else:
+                return 'unknown type'
+
+        return vpath
 
 
-# Press the green button in the gutter to run the script.
+class Band(object):
+    @cherrypy.expose
+    def index(self, name):
+        return  # f'SELECT * FROM {name}'
+
+
+class Album(object):
+    @cherrypy.expose
+    def index(self, artist, title):
+        return 'About %s by %s...' % (artist, title)
+
+
+class Song(object):
+    @cherrypy.expose
+    def index(self, artist, name):
+        return 'Song %s by %s...' % (artist, name)
+
+
+class Everyone(object):
+    @cherrypy.expose
+    def index(self):
+        return  # SELECT * FROM artists
+
+
+class Search(object):
+    @cherrypy.expose
+    def index(self):
+        pass
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    cherrypy.quickstart(Router())
